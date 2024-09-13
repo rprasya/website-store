881fa4d252827440147f45bee4d7230dbf5f6329
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-09-13 16:15:36 +0700

    feat: display products as product cards using data from admin section

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,22 @@
 import getBanner from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
 import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const banner = await getBanner("50356a17-d6eb-4ed4-b7ae-42e70d9734b8")
+  const products = await getProducts({ isFeatured: true });
+  const banner = await getBanner("336ac1fc-81cf-4414-aa1c-e7c85b2d6851");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produk Unggulan" items={products} />
+        </div>
       </div>
     </Container>
   );
---
+++ components/ui/no-results.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+    return (
+        <div className="flex items-center justify-center w-full h-full text-neutral-500">
+            Tidak ada produk yang ditemukan.
+        </div>
+    );
+}
+ 
+export default NoResults;
